64883042da947cf49a793861d60a601cc9ba9b0e
Devin Gillogly <virlym@gmail.com>
2020-11-06 20:19:59 -0800

    updated readme

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export default {
+const API = {
   search: function() {
     return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
+
+export default API;
