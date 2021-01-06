174075890d4db935fcc74d053f41c6392142a063
Devin Gillogly <virlym@gmail.com>
2021-01-06 14:50:25 -0800

    basic movie search bar field

--- src/pages/movieSearch/movieSearch.js
+++ src/pages/movieSearch/movieSearch.js
@@ -2,12 +2,45 @@ import React from "react";
 
 function MovieSearch() {
   return (
-    <div>
-      <h1>Movies Page</h1>
-      <p>
-        Hello
-      </p>
-    </div>
+    <div className="container">
+
+        <div className="row">
+          <div className="col-12">
+            <h1>
+              Movie Search
+            </h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <div className="input-group input-group-lg" style={{marginTop:"20px", textAlign:"center"}}>
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-lg">Search</span>
+              </div>
+              <input
+                className="form-control"
+                id="movieSearch"
+                // value={this.state.searchTerm}
+                name="searchTerm"
+                // onChange={this.handleInputChange.bind(this)}
+                type="text"
+                placeholder="Movie Title"
+              />
+            </div>
+          </div>
+        </div>
+
+        <br />
+
+        <div className="row">
+          <div className="col-12">
+            {/* <table id="myTable">
+              <Table table={this.state.tableList.data} sort={this.sortTable.bind(this)}/>
+            </table> */}
+          </div>
+        </div>
+      </div>
   );
 }
 
