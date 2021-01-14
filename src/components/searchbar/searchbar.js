fbe9614ce0ce589794dd26c09b35f7a37c2429d6
Devin Gillogly <virlym@gmail.com>
2021-01-14 02:54:03 -0800

    updated search bar

---
+++ src/components/searchbar/searchbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./searchbar.css";
+
+function Searchbar(props) {
+    return (
+
+        <div className="row">
+            <div className="col-12">
+                <form onSubmit={props.searchAPI} className="input-group input-group-lg search-style">
+                <select className="input-group-prepend" name="searchType" onChange={props.handleSearchInputChange} value={props.searchState.searchType}> 
+                    { props.searchType === "movie"
+                    ?       <>
+                            <option value="all"> All </option>
+                            <option value="movies"> Movies </option>
+                            <option value ="series"> Series </option>
+                            </>
+                    : props.searchType === "book"
+                    ?
+                            <>
+                            <option value="all"> All </option>
+                            <option value="title"> Title </option>
+                            <option value ="author"> Author </option>
+                            <option value="publisher"> Publisher </option>
+                            <option value ="subject"> Subject </option>
+                            <option value="isbn"> ISBN </option>
+                            </>
+                    :
+                            <>
+                            <option value="artist"> Artist </option>
+                            <option value="album"> Album </option>
+                            <option value ="song"> Song </option>
+                            </>
+                    }
+                    </select>
+                    <input
+                        className="form-control"
+                        value={props.searchState.searchTerm || ""}
+                        name="searchTerm"
+                        onChange={props.handleSearchInputChange}
+                        type="text"
+                        placeholder="Search Term"
+                    />
+                    <input type="submit" className="input-group-append input-group-text" value="Search" />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Searchbar;
--- src/pages/movieSearch/movieSearch.js
+++ src/pages/movieSearch/movieSearch.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./movieSearch.css";
+import API from "../../utils/MovieAPI.js";
+import Searchbar from "../../components/searchbar/searchbar.js"
 
 function MovieSearch() {
+  const [searchState, setSearchState] = useState({
+    searchTerm: "",
+    searchType: "",
+    searchResults: []
+    // {id, title, year, poster, nominated (T/F)}
+  });
+
+    // user nominations
+    const [nominationState, setNominationState] = useState({
+      nominations: []
+      // {id, title, year, poster}
+    });
+
+  function testOutput (data) {
+    console.log(data);
+  }
+
+  function searchMovies(event){
+    event.preventDefault();
+    console.log("hi");
+    console.log(searchState.searchTerm);
+    console.log(searchState.searchType);
+    // API.search(searchState.searchTerm)
+    //     .then(function (res){
+    //       if(res.data.Response === "True"){
+    //         console.log(res.data);
+    //         let movieList = [];
+    //         for(let i = 0; i < res.data.Search.length; i++){
+    //           if(res.data.Search[i].Type === "movie"){
+    //             let isNominated = false;
+    //             for(let j = 0; j < nominationState.nominations.length; j++){
+    //               if(nominationState.nominations[j].id === res.data.Search[i].imdbID){
+    //                 isNominated = true;
+    //               }
+    //             }
+    //             let imgLink = res.data.Search[i].Poster;
+    //             if(imgLink === "N/A"){
+    //               imgLink = "https://123moviesfree.zone/no-poster.png"
+    //             }
+    //             movieList.push({id: res.data.Search[i].imdbID, title: res.data.Search[i].Title, year: res.data.Search[i].Year, poster: imgLink, nominated: isNominated});
+    //           }
+    //         }
+    //         setSearchState({...searchState, searchResults: movieList});
+    //       }
+    //     })
+    //     .catch(err => console.log("error :", err));
+  }
+
+  // track search field input
+  function handleMovieSearchInputChange(event){
+    const value = event.target.value;
+    const name = event.target.name;
+    setSearchState({...searchState, [name]: value});
+  }
+
   return (
     <div className="container">
 
@@ -13,24 +70,7 @@ function MovieSearch() {
           </div>
         </div>
 
-        <div className="row">
-          <div className="col-12">
-            <div className="input-group input-group-lg" style={{marginTop:"20px", textAlign:"center"}}>
-              <div className="input-group-prepend">
-                <span className="input-group-text" id="inputGroup-sizing-lg">Search</span>
-              </div>
-              <input
-                className="form-control"
-                id="movieSearch"
-                // value={this.state.searchTerm}
-                name="searchTerm"
-                // onChange={this.handleInputChange.bind(this)}
-                type="text"
-                placeholder="Movie Title"
-              />
-            </div>
-          </div>
-        </div>
+        <Searchbar searchAPI={searchMovies} handleSearchInputChange={handleMovieSearchInputChange} searchState={searchState} searchType="movie"/>
 
         <br />
 
