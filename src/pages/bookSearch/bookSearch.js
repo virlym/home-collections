6a18747908e99fef608d71826e4eca9e1da4d2aa
Devin Gillogly <virlym@gmail.com>
2021-02-02 04:35:14 -0800

    fixed unique filteirng for books and movies

--- src/pages/bookSearch/bookSearch.js
+++ src/pages/bookSearch/bookSearch.js
@@ -16,168 +16,168 @@ function BookSearch(props) {
   });
 
   function getAllSearch(results, index, rawResults) {
-    if(searchState.searchType === "title"){
+    if (searchState.searchType === "title") {
       API.titleSearch(searchState.searchTerm, index)
-        .then(function (res){
-          if(res.data.totalItems > 0){
-            if(rawResults.length > 0){
+        .then(function (res) {
+          if (res.data.totalItems > 0) {
+            if (rawResults.length > 0) {
               rawResults = rawResults.concat(res.data.items);
             }
-            else{
+            else {
               rawResults = res.data.items;
             }
-            if(res.data.items !== undefined){
+            if (res.data.items !== undefined) {
               results = parseInt(results) + parseInt(res.data.items.length);
             }
             index = index + 40;
-            if((results < parseInt(res.data.totalItems)) && ( res.data.totalItems / 40 > 1 ) && (res.data.items !== undefined)){
+            if ((results < parseInt(res.data.totalItems)) && (res.data.totalItems / 40 > 1) && (res.data.items !== undefined)) {
               getAllSearch(results, index, rawResults);
             }
-            else{
+            else {
               sortResults(rawResults);
             }
           }
-          else{
+          else {
             setSearchState({ ...searchState, searching: false, searchResults: [] });
           }
         })
         .catch(err => console.log("error :", err));
     }
-    else if(searchState.searchType === "author"){
+    else if (searchState.searchType === "author") {
       API.authorSearch(searchState.searchTerm, index)
-        .then(function (res){
+        .then(function (res) {
           setSearchState({ ...searchState, searching: true });
-          if(res.data.totalItems > 0){
-            if(rawResults.length > 0){
+          if (res.data.totalItems > 0) {
+            if (rawResults.length > 0) {
               rawResults = rawResults.concat(res.data.items);
             }
-            else{
+            else {
               rawResults = res.data.items;
             }
-            if(res.data.items !== undefined){
+            if (res.data.items !== undefined) {
               results = parseInt(results) + parseInt(res.data.items.length);
             }
             index = index + 40;
-            if((results < parseInt(res.data.totalItems)) && ( res.data.totalItems / 40 > 1 ) && (res.data.items !== undefined)){
+            if ((results < parseInt(res.data.totalItems)) && (res.data.totalItems / 40 > 1) && (res.data.items !== undefined)) {
               getAllSearch(results, index, rawResults);
             }
-            else{
+            else {
               sortResults(rawResults);
             }
           }
-          else{
+          else {
             setSearchState({ ...searchState, searching: false, searchResults: [] });
           }
         })
         .catch(err => console.log("error :", err));
     }
-    else if(searchState.searchType === "publisher"){
+    else if (searchState.searchType === "publisher") {
       API.publisherSearch(searchState.searchTerm, index)
-        .then(function (res){
+        .then(function (res) {
           setSearchState({ ...searchState, searching: true });
-          if(res.data.totalItems > 0){
-            if(rawResults.length > 0){
+          if (res.data.totalItems > 0) {
+            if (rawResults.length > 0) {
               rawResults = rawResults.concat(res.data.items);
             }
-            else{
+            else {
               rawResults = res.data.items;
             }
-            if(res.data.items !== undefined){
+            if (res.data.items !== undefined) {
               results = parseInt(results) + parseInt(res.data.items.length);
             }
             index = index + 40;
-            if((results < parseInt(res.data.totalItems)) && ( res.data.totalItems / 40 > 1 ) && (res.data.items !== undefined)){
+            if ((results < parseInt(res.data.totalItems)) && (res.data.totalItems / 40 > 1) && (res.data.items !== undefined)) {
               getAllSearch(results, index, rawResults);
             }
-            else{
+            else {
               sortResults(rawResults);
             }
           }
-          else{
+          else {
             setSearchState({ ...searchState, searching: false, searchResults: [] });
           }
         })
         .catch(err => console.log("error :", err));
     }
-    else if(searchState.searchType === "subject"){
+    else if (searchState.searchType === "subject") {
       API.subjectSearch(searchState.searchTerm, index)
-        .then(function (res){
+        .then(function (res) {
           setSearchState({ ...searchState, searching: true });
-          if(res.data.totalItems > 0){
-            if(rawResults.length > 0){
+          if (res.data.totalItems > 0) {
+            if (rawResults.length > 0) {
               rawResults = rawResults.concat(res.data.items);
             }
-            else{
+            else {
               rawResults = res.data.items;
             }
-            if(res.data.items !== undefined){
+            if (res.data.items !== undefined) {
               results = parseInt(results) + parseInt(res.data.items.length);
             }
             index = index + 40;
-            if((results < parseInt(res.data.totalItems)) && ( res.data.totalItems / 40 > 1 ) && (res.data.items !== undefined)){
+            if ((results < parseInt(res.data.totalItems)) && (res.data.totalItems / 40 > 1) && (res.data.items !== undefined)) {
               getAllSearch(results, index, rawResults);
             }
-            else{
+            else {
               sortResults(rawResults);
             }
           }
-          else{
+          else {
             setSearchState({ ...searchState, searching: false, searchResults: [] });
           }
         })
         .catch(err => console.log("error :", err));
     }
-    else if(searchState.searchType === "isbn"){
+    else if (searchState.searchType === "isbn") {
       API.isbnSearch(searchState.searchTerm, index)
-        .then(function (res){
+        .then(function (res) {
           setSearchState({ ...searchState, searching: true });
-          if(res.data.totalItems > 0){
-            if(rawResults.length > 0){
+          if (res.data.totalItems > 0) {
+            if (rawResults.length > 0) {
               rawResults = rawResults.concat(res.data.items);
             }
-            else{
+            else {
               rawResults = res.data.items;
             }
-            if(res.data.items !== undefined){
+            if (res.data.items !== undefined) {
               results = parseInt(results) + parseInt(res.data.items.length);
             }
             index = index + 40;
-            if((results < parseInt(res.data.totalItems)) && ( res.data.totalItems / 40 > 1 ) && (res.data.items !== undefined)){
+            if ((results < parseInt(res.data.totalItems)) && (res.data.totalItems / 40 > 1) && (res.data.items !== undefined)) {
               getAllSearch(results, index, rawResults);
             }
-            else{
+            else {
               sortResults(rawResults);
             }
           }
-          else{
+          else {
             setSearchState({ ...searchState, searching: false, searchResults: [] });
           }
         })
         .catch(err => console.log("error :", err));
     }
-    else{
+    else {
       API.generalSearch(searchState.searchTerm, index)
-        .then(function (res){
+        .then(function (res) {
           setSearchState({ ...searchState, searching: true });
-          if(res.data.totalItems > 0){
-            if(rawResults.length > 0){
+          if (res.data.totalItems > 0) {
+            if (rawResults.length > 0) {
               rawResults = rawResults.concat(res.data.items);
             }
-            else{
+            else {
               rawResults = res.data.items;
             }
-            if(res.data.items !== undefined){
+            if (res.data.items !== undefined) {
               results = parseInt(results) + parseInt(res.data.items.length);
             }
             index = index + 40;
-            if((results < parseInt(res.data.totalItems)) && ( res.data.totalItems / 40 > 1 ) && (res.data.items !== undefined)){
+            if ((results < parseInt(res.data.totalItems)) && (res.data.totalItems / 40 > 1) && (res.data.items !== undefined)) {
               getAllSearch(results, index, rawResults);
             }
-            else{
+            else {
               sortResults(rawResults);
             }
           }
-          else{
+          else {
             setSearchState({ ...searchState, searching: false, searchResults: [] });
           }
         })
@@ -185,31 +185,39 @@ function BookSearch(props) {
     }
   }
 
-  function sortResults(rawResults){
+  function sortResults(rawResults) {
     let bookList = [];
     rawResults = [...new Set(rawResults)];
-            for(let i = 0; i < rawResults.length; i++){
-              if(rawResults[i] !== undefined){
-                let hasBook = false;
-                for(let j = 0; j < props.userBooks.length; j++){
-                  if(props.userBooks[j].id === rawResults[i].id){
-                    hasBook = true;
-                  }
-                }
-                let imgLink = "";
-                if(rawResults[i].volumeInfo.imageLinks !== undefined){
-                  imgLink = rawResults[i].volumeInfo.imageLinks.thumbnail;
-                }
-                else{
-                  imgLink = "https://hrce.insigniails.com/Library/images/~imageCT459526.JPG"
-                }
-                bookList.push({id: rawResults[i].id, title: rawResults[i].volumeInfo.title, subtitle: rawResults[i].volumeInfo.subtitle, authors: rawResults[i].volumeInfo.authors, cover: imgLink, publisher: rawResults[i].volumeInfo.publisher, published: rawResults[i].volumeInfo.publishedDate, owned: hasBook});
-            }
-            }
-            setSearchState({...searchState, searching: false, searchResults: bookList});
+    for (let i = 0; i < rawResults.length; i++) {
+      if (rawResults[i] !== undefined) {
+        let hasBook = false;
+        for (let j = 0; j < props.userBooks.length; j++) {
+          if (props.userBooks[j].id === rawResults[i].id) {
+            hasBook = true;
+          }
+        }
+        let imgLink = "";
+        if (rawResults[i].volumeInfo.imageLinks !== undefined) {
+          imgLink = rawResults[i].volumeInfo.imageLinks.thumbnail;
+        }
+        else {
+          imgLink = "https://hrce.insigniails.com/Library/images/~imageCT459526.JPG"
+        }
+        bookList.push({ id: rawResults[i].id, title: rawResults[i].volumeInfo.title, subtitle: rawResults[i].volumeInfo.subtitle, authors: rawResults[i].volumeInfo.authors, cover: imgLink, publisher: rawResults[i].volumeInfo.publisher, published: rawResults[i].volumeInfo.publishedDate, owned: hasBook });
+      }
+    }
+    let uniqueBookList = Array.from(new Set(bookList.map(function (data) {
+      return data.id
+    })))
+      .map(function (id) {
+        return bookList.find(function (data) {
+          return data.id === id
+        })
+      });
+    setSearchState({ ...searchState, searching: false, searchResults: uniqueBookList });
   }
 
-  function searchBooks(event){
+  function searchBooks(event) {
     event.preventDefault();
     let results = 0;
     let index = 0;
@@ -219,44 +227,45 @@ function BookSearch(props) {
   }
 
   // track search field input
-  function handleBookSearchInputChange(event){
+  function handleBookSearchInputChange(event) {
     const value = event.target.value;
     const name = event.target.name;
-    setSearchState({...searchState, [name]: value});
+    setSearchState({ ...searchState, [name]: value });
   }
 
-  function addToCollection(){
+  function addToCollection() {
 
   }
 
   return (
     <div className="container">
 
-        <div className="row">
-          <div className="col-12">
-            <h1>
-              Book Search
+      <div className="row">
+        <div className="col-12">
+          <h1>
+            Book Search
             </h1>
-          </div>
         </div>
+      </div>
 
-        <Searchbar searchAPI={searchBooks} handleSearchInputChange={handleBookSearchInputChange} searchState={searchState} searchType="book"/>
+      <Searchbar searchAPI={searchBooks} handleSearchInputChange={handleBookSearchInputChange} searchState={searchState} searchType="book" />
 
-        <br />
+      <br />
 
-        <div className="row">
+      <div className="row">
         <div className="col-12 border border-info rounded-top rounded-bottom display-box">
           <h3 className="section-head"> Search Results </h3>
           {searchState.searching === true
-          ? <Searching />
-          : searchState.searchResults.length > 0
-            ? searchState.searchResults.map(function(searchArray){
-              return <BookSearchResults searchResults={searchArray} key={searchArray.id} addToCollection={addToCollection} loggedIn={props.loggedIn}/>})
-            : <NoSearchResults />
+            ? <Searching />
+            : searchState.searchResults.length > 0
+              ? searchState.searchResults.map(function (searchArray) {
+                return <BookSearchResults searchResults={searchArray} key={searchArray.id} addToCollection={addToCollection} loggedIn={props.loggedIn} />
+              })
+              : <NoSearchResults />
           }
         </div>
-        </div>
       </div>
+    </div>
   );
 }
 
--- src/pages/movieSearch/movieSearch.js
+++ src/pages/movieSearch/movieSearch.js
@@ -17,86 +17,95 @@ function MovieSearch(props) {
 
   function getAllSearch(results, page, rawResults) {
     API.search(searchState.searchTerm, page)
-        .then(function (res){
-          if(res.data.Response === "True"){
-            if(rawResults.length > 0){
-              rawResults = rawResults.concat(res.data.Search);
-            }
-            else{
-              rawResults = res.data.Search;
-            }
-            results = parseInt(results) + parseInt(res.data.Search.length);
-            page++;
-            if(results < parseInt(res.data.totalResults)){
-              getAllSearch(results, page, rawResults);
-            }
-            else{
-              sortResults(rawResults);
-            }
+      .then(function (res) {
+        if (res.data.Response === "True") {
+          if (rawResults.length > 0) {
+            rawResults = rawResults.concat(res.data.Search);
           }
-          else{
-            setSearchState({ ...searchState, searching: false, searchResults: [] });
+          else {
+            rawResults = res.data.Search;
           }
-        })
-        .catch(err => console.log("error :", err));
+          results = parseInt(results) + parseInt(res.data.Search.length);
+          page++;
+          if (results < parseInt(res.data.totalResults)) {
+            getAllSearch(results, page, rawResults);
+          }
+          else {
+            sortResults(rawResults);
+          }
+        }
+        else {
+          setSearchState({ ...searchState, searching: false, searchResults: [] });
+        }
+      })
+      .catch(err => console.log("error :", err));
   }
 
-  function sortResults(rawResults){
+  function sortResults(rawResults) {
     let movieList = [];
-            for(let i = 0; i < rawResults.length; i++){
-              if(searchState.searchType === "movies"){
-                if(rawResults[i].Type === "movie"){
-                  let hasMovie = false;
-                  for(let j = 0; j < props.userMovies.length; j++){
-                    if(props.userMovies[j].id === rawResults[i].imdbID){
-                      hasMovie = true;
-                    }
-                  }
-                  let imgLink = rawResults[i].Poster;
-                  if(imgLink === "N/A"){
-                    imgLink = "https://123moviesfree.zone/no-poster.png"
-                  }
-                  movieList.push({id: rawResults[i].imdbID, title: rawResults[i].Title, year: rawResults[i].Year, poster: imgLink, owned: hasMovie});
-                }
-              }
-
-              else if(searchState.searchType === "series"){
-                if(rawResults[i].Type === "series"){
-                  let hasMovie = false;
-                  for(let j = 0; j < props.userMovies.length; j++){
-                    if(props.userMovies[j].id === rawResults[i].imdbID){
-                      hasMovie = true;
-                    }
-                  }
-                  let imgLink = rawResults[i].Poster;
-                  if(imgLink === "N/A"){
-                    imgLink = "https://123moviesfree.zone/no-poster.png"
-                  }
-                  movieList.push({id: rawResults[i].imdbID, title: rawResults[i].Title, year: rawResults[i].Year, poster: imgLink, owned: hasMovie});
-                }
-              }
-
-              else{
-                if((rawResults[i].Type === "movie")||(rawResults[i].Type === "series")){
-                  let hasMovie = false;
-                  for(let j = 0; j < props.userMovies.length; j++){
-                    if(props.userMovies[j].id === rawResults[i].imdbID){
-                      hasMovie = true;
-                    }
-                  }
-                  let imgLink = rawResults[i].Poster;
-                  if(imgLink === "N/A"){
-                    imgLink = "https://123moviesfree.zone/no-poster.png"
-                  }
-                  movieList.push({id: rawResults[i].imdbID, title: rawResults[i].Title, year: rawResults[i].Year, poster: imgLink, owned: hasMovie});
-                }
-              }
-
+    for (let i = 0; i < rawResults.length; i++) {
+      if (searchState.searchType === "movies") {
+        if (rawResults[i].Type === "movie") {
+          let hasMovie = false;
+          for (let j = 0; j < props.userMovies.length; j++) {
+            if (props.userMovies[j].id === rawResults[i].imdbID) {
+              hasMovie = true;
             }
-            setSearchState({...searchState, searching: false, searchResults: movieList});
+          }
+          let imgLink = rawResults[i].Poster;
+          if (imgLink === "N/A") {
+            imgLink = "https://123moviesfree.zone/no-poster.png"
+          }
+          movieList.push({ id: rawResults[i].imdbID, title: rawResults[i].Title, year: rawResults[i].Year, poster: imgLink, owned: hasMovie });
+        }
+      }
+
+      else if (searchState.searchType === "series") {
+        if (rawResults[i].Type === "series") {
+          let hasMovie = false;
+          for (let j = 0; j < props.userMovies.length; j++) {
+            if (props.userMovies[j].id === rawResults[i].imdbID) {
+              hasMovie = true;
+            }
+          }
+          let imgLink = rawResults[i].Poster;
+          if (imgLink === "N/A") {
+            imgLink = "https://123moviesfree.zone/no-poster.png"
+          }
+          movieList.push({ id: rawResults[i].imdbID, title: rawResults[i].Title, year: rawResults[i].Year, poster: imgLink, owned: hasMovie });
+        }
+      }
+
+      else {
+        if ((rawResults[i].Type === "movie") || (rawResults[i].Type === "series")) {
+          let hasMovie = false;
+          for (let j = 0; j < props.userMovies.length; j++) {
+            if (props.userMovies[j].id === rawResults[i].imdbID) {
+              hasMovie = true;
+            }
+          }
+          let imgLink = rawResults[i].Poster;
+          if (imgLink === "N/A") {
+            imgLink = "https://123moviesfree.zone/no-poster.png"
+          }
+          movieList.push({ id: rawResults[i].imdbID, title: rawResults[i].Title, year: rawResults[i].Year, poster: imgLink, owned: hasMovie });
+        }
+      }
+
+    }
+    
+    let uniqueMovieList = Array.from(new Set(movieList.map(function (data) {
+      return data.id
+    })))
+      .map(function (id) {
+        return movieList.find(function (data) {
+          return data.id === id
+        })
+      });
+    setSearchState({ ...searchState, searching: false, searchResults: uniqueMovieList });
   }
 
-  function searchMovies(event){
+  function searchMovies(event) {
     event.preventDefault();
     let results = 0;
     let page = 1;
@@ -106,44 +115,45 @@ function MovieSearch(props) {
   }
 
   // track search field input
-  function handleMovieSearchInputChange(event){
+  function handleMovieSearchInputChange(event) {
     const value = event.target.value;
     const name = event.target.name;
-    setSearchState({...searchState, [name]: value});
+    setSearchState({ ...searchState, [name]: value });
   }
 
-  function addToCollection(){
+  function addToCollection() {
 
   }
 
   return (
     <div className="container">
 
-        <div className="row">
-          <div className="col-12">
-            <h1>
-              Movie Search
+      <div className="row">
+        <div className="col-12">
+          <h1>
+            Movie Search
             </h1>
-          </div>
         </div>
+      </div>
 
-        <Searchbar searchAPI={searchMovies} handleSearchInputChange={handleMovieSearchInputChange} searchState={searchState} searchType="movie"/>
+      <Searchbar searchAPI={searchMovies} handleSearchInputChange={handleMovieSearchInputChange} searchState={searchState} searchType="movie" />
 
-        <br />
+      <br />
 
-        <div className="row">
+      <div className="row">
         <div className="col-12 border border-info rounded-top rounded-bottom display-box">
           <h3 className="section-head"> Search Results </h3>
           {searchState.searching === true
             ? <Searching />
             : searchState.searchResults.length > 0
-              ? searchState.searchResults.map(function(searchArray){
-                return <MovieSearchResults searchResults={searchArray} key={searchArray.id} addToCollection={addToCollection} loggedIn={props.loggedIn}/>})
+              ? searchState.searchResults.map(function (searchArray) {
+                return <MovieSearchResults searchResults={searchArray} key={searchArray.id} addToCollection={addToCollection} loggedIn={props.loggedIn} />
+              })
               : <NoSearchResults />
           }
         </div>
-        </div>
       </div>
+    </div>
   );
 }
 
