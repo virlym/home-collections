081ff9eea8a7c4bdd3dde7953a4c02af121d67a6
Devin Gillogly <virlym@gmail.com>
2021-02-12 11:56:27 -0800

    testing cors mode

--- src/utils/API.js
+++ src/utils/API.js
@@ -22,8 +22,7 @@ const API = {
     return fetch(`${URL_PREFIX}api/users/login`, {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': `${URL_PREFIX}api/users/login`
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(info)
     }).then(function (res) {
