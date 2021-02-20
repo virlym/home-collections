d0e045f7ee02af68765db59bba8aac2a3799ca96
Devin Gillogly <virlym@gmail.com>
2021-02-19 19:48:41 -0800

    landing page done

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -25,35 +25,41 @@ function Landing(props) {
       <h1>Home Collections</h1>
       <div className="container icon-container">
         <div className="row">
-          <div className="col-xs-12 col-sm-4 d-flex justify-content-center">
+          <div className="col-xs-12 col-sm-4 d-flex justify-content-center icon-space">
             <button onClick={function(){searchButton("books")}}>
               <img className="icon-size" src="https://www.shareicon.net/data/128x128/2015/12/31/696126_books_512x512.png" alt="book-icon"/>
+              <figcaption>Book Search</figcaption>
             </button>
           </div>
-          <div className="col-xs-12 col-sm-4 d-flex justify-content-center">
+          <div className="col-xs-12 col-sm-4 d-flex justify-content-center icon-space">
             <button onClick={function(){searchButton("movies")}}>
-              <img className="icon-size" src="https://lh6.googleusercontent.com/proxy/7XiOoRWE0uH5Ckp0CYV6wc8mTBf5FqQQi6GzfkBWw88iTcuvgtCnG2nqqy1lkcyvcJzi_q2WcO-wteXjCXQtkl3FwKvpyv0gpOnHokk=s0-d" alt="book-icon"/>
+              <img className="icon-size" src="https://lh6.googleusercontent.com/proxy/7XiOoRWE0uH5Ckp0CYV6wc8mTBf5FqQQi6GzfkBWw88iTcuvgtCnG2nqqy1lkcyvcJzi_q2WcO-wteXjCXQtkl3FwKvpyv0gpOnHokk=s0-d" alt="movie-icon"/>
+              <figcaption>Movie Search</figcaption>
             </button>
           </div>
-          <div className="col-xs-12 col-sm-4 d-flex justify-content-center">
+          <div className="col-xs-12 col-sm-4 d-flex justify-content-center icon-space">
             <button onClick={function(){searchButton("music")}}>
-              <img className="icon-size" src="https://www.shareicon.net/data/128x128/2016/03/28/741104_multimedia_512x512.png" alt="book-icon"/>
+              <img className="icon-size" src="https://www.shareicon.net/data/128x128/2016/03/28/741104_multimedia_512x512.png" alt="music-icon"/>
+              <figcaption>Music Search</figcaption>
             </button>
           </div>
         </div>
         <br /><br /><br />
-      <p>
+      <p className="app-info">
         Home Collections is a web app designed to help people keep track of their entertainment media.
       </p>
-      <p>
+      <p className="app-info">
         Anyone may utilize the search engines to look up media, but in order to save anything, you need to create an account.
       </p>
-      <p>
-        <ul>
-          <li>The book search engine is powered by Google Books.</li>
-          <li>The movie search engine is powered by OMDB.</li>
-          <li>The music search engine is powered by Spotify.</li>
-        </ul>
+      <br /><br />
+      <p className="app-info">
+        The book search engine is powered by Google Books.
+      </p>
+      <p className="app-info">
+        The movie search engine is powered by OMDB.
+      </p>
+      <p className="app-info">
+        The music search engine is powered by Spotify.
       </p>
       </div>
     </div>
