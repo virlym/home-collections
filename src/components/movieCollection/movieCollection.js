1fc2d132ce4fe3484e2bd62a7d9570e943cfc0b1
Devin Gillogly <virlym@gmail.com>
2021-02-24 11:35:40 -0800

    comment icon in movie collection

--- src/components/movieCollection/movieCollection.js
+++ src/components/movieCollection/movieCollection.js
@@ -18,7 +18,7 @@ function MovieCollection(props) {
 
   function dvdCheckChange(event) {
     let value = false;
-    if(event.target.value === "false"){
+    if (event.target.value === "false") {
       value = true
     }
     const comment = props.collection.comments || "";
@@ -36,7 +36,7 @@ function MovieCollection(props) {
 
   function blueRayCheckChange(event) {
     let value = false;
-    if(event.target.value === "false"){
+    if (event.target.value === "false") {
       value = true
     }
     const comment = props.collection.comments || "";
@@ -74,9 +74,21 @@ function MovieCollection(props) {
         <div className="col-lg-4 col-md-12 my-auto">
           <div className="row description-row">
             <div className="col-12">
-              <p> 
+              <p>
                 {props.collection.title} ({props.collection.year}) <br />
+                {props.collection.comments
+                  ?
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                    <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                  </svg>
+                  :
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                  </svg>
+                }
               </p>
+
               <div className="form-check form-check-inline">
                 <input className="form-check-input" type="checkbox" id="dvdCheckBox" value={props.collection.dvd} name="dvd" onChange={dvdCheckChange} checked={props.collection.dvd} />
                 <label className="form-check-label"> DVD </label>
