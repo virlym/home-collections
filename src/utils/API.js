af78bd88ead0ec6b625278d9f71d466a1c0fd087
Devin Gillogly <virlym@gmail.com>
2021-03-22 16:33:53 -0700

    production url

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
-const URL_PREFIX = "http://localhost:8080/"
-// const URL_PREFIX = "https://home-collections-api.herokuapp.com/"
+// const URL_PREFIX = "http://localhost:8080/"
+const URL_PREFIX = "https://home-collections-api.herokuapp.com/"
 
 
 const API = {
