7f35585dc3d4c307c9055e7f8e2d72dc7bcc5192
Devin Gillogly <virlym@gmail.com>
2021-04-13 00:53:18 -0700

    added comments to bookCollection component

--- src/components/bookCollection/bookCollection.js
+++ src/components/bookCollection/bookCollection.js
@@ -3,11 +3,20 @@ import "./bookCollection.css";
 import API from "../../utils/API.js";
 
 function BookCollection(props) {
+  // props required from the userState:
+  // session token, and the setUserState method
+  
+  // props required from the passed collection:
+  // cover, title, subtitle (if available), published date (if available), authors, and the book id
 
+  // function that will remove the book from the user's collection
   function removeFromCollection() {
+    //API call to remove the book from the user's collection in the database
     API.removeBook(props.userState.token, props.collection.id).then(function (delBook) {
+      //If a book was removed, refresh the user's book list in the app
       if (delBook) {
         API.getUserBooks(props.userState.token).then(function (userBooks) {
+          //If the user has books after the refresh, store them in the user state hook
           if (userBooks) {
             props.setUserState({ ...props.userState, books: userBooks });
           }
@@ -15,7 +24,11 @@ function BookCollection(props) {
       }
     });
   }
-
+  // visual:
+  // cover image    - title                        - remove button
+  //                - subtitle (if present)
+  //                - year published (if present)
+  //                - author
   return (
     <div>
       <div className="row">
@@ -26,10 +39,12 @@ function BookCollection(props) {
           <div className="row description-row">
             <div className="col-12">
               <p> {props.collection.title} <br />
+                {/* if the book has a subtitle, display it */}
                 {props.collection.subtitle
                   ? <> {props.collection.subtitle} <br /> </>
                   : null
                 }
+                {/* if the book has a publication date, display the year */}
                 {props.collection.published
                   ? <> ({props.collection.published.substring(0, 4)}) <br /> </>
                   : null
@@ -40,6 +55,7 @@ function BookCollection(props) {
           </div>
         </div>
         <div className="col-lg-4 col-md-12 my-auto">
+          {/* the remove button will call an API function to remove it from the user in the database when pressed */}
           <button onClick={function () { removeFromCollection() }}> Remove </button>
         </div>
       </div>
